500f9e81371057db3c953263ebaf435677c721b2
zhangyongchang1 <zhangyongchang1@100tal.com>
2018-02-13 21:05:23 +0800

    小型购物车完成

--- pyTestList/week_02/day_06/shopping.py
+++ pyTestList/week_02/day_06/shopping.py
@@ -1,31 +1,50 @@
-#_author: earl
-#_data: 18/02/08
+# _author: earl
+# _data: 18/02/08
 # -*- coding: utf-8 -*
 
-#购物车
-goodsList = ['pengguo','pad','android','apple']
-priceList = ['100','2000','1000','3000']
+# 购物车
+goodsList = ['pingguo', 'pad', 'android', 'apple']
+priceList = ['100', '2000', '1000', '3000']
 selectGoodsList = []
 priceTotal = 10000
 selectGoodsPriceTotal = 0
 
-for index,val  in enumerate(goodsList):
+print('---------- 货物价格如下 ------------')
+for index, val in enumerate(goodsList):
     msg = '''
-    '---------- 货物 (%s) 价格 ------------'
+    number : %s
     name : %s
     price  : %s
     ''' % (str(index), val, priceList[index])
     print(msg)
 
 while True:
-    inputNber = int(input('请输入要购买的商品序号'))
-    if inputNber > len(goodsList):
+    inputNber = input('请输入要购买的商品序号')
+    if not inputNber:
+        inputNber = '100'
+    inputNber = int(inputNber)
+    if not inputNber or inputNber > len(goodsList)-1:
         print('暂无此商品')
         continue
     else:
         if selectGoodsPriceTotal > priceTotal:
-            print('你的余额不足')
-            selectGoodsPriceTotal+=priceList[inputNber]
+            print('你的钱已经花完，总得商品如下')
+            selectGoodsList.pop(len(selectGoodsList)-1)
+            for index, val in enumerate(selectGoodsList):
+                good = '''
+                name : %s
+                price  : %s
+                ''' % (goodsList[val], priceList[val])
+                print(good)
+            break
+        else:
+            selectGoodsList.append(inputNber)
+            selectGoodsPriceTotal += int(priceList[inputNber])
 
-
-
+            print('---------- 您购买的的商品如下 ------------')
+            for index, val in enumerate(selectGoodsList):
+                good = '''
+                           name : %s
+                           price  : %s
+                           ''' % (goodsList[val], priceList[val])
+                print(good)
